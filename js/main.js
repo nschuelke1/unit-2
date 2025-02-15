12c0e3d2bea1dad618cae783d20a2e3dac0dd1a4
nschuelke1 <ndschuelke@wisc.edu>
2025-02-15 14:24:17 -0600

    2013 population proportional symbols

--- js/main.js
+++ js/main.js
@@ -1,57 +1,104 @@
-// Create a function to attach popups to each mapped feature
-function onEachFeature(feature, layer) {
-    // Create HTML string with all properties
-    var popupContent = "";
-    if (feature.properties) {
-        // Loop to add feature property names and values to HTML string
-        for (var property in feature.properties){
-            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
-        }
-        layer.bindPopup(popupContent);
-    }
-}
+// Declare map variable globally so all functions have access
+var map;
+var minValue;
 
-// Define marker options for GeoJSON point features
-var geojsonMarkerOptions = {
-    radius: 8,
-    fillColor: "#ff7800",
-    color: "#000",
-    weight: 1,
-    opacity: 1,
-    fillOpacity: 0.8
-};
-
-// Function to retrieve the data and place it on the map
-function getData(map){
-    // Load the data
-    fetch("data/StatePopChange.geojson")
-        .then(function(response){
-            return response.json();
-        })
-        .then(function(json){
-            // Create a Leaflet GeoJSON layer and add it to the map
-            L.geoJSON(json, {
-                pointToLayer: function (feature, latlng) {
-                    return L.circleMarker(latlng, geojsonMarkerOptions);
-                },
-                onEachFeature: onEachFeature
-            }).addTo(map);
-        });
-}
-
-// Function to instantiate the Leaflet map
+// Step 1: Create the map
 function createMap() {
     // Create the map and center it over the United States with zoom level 4
-    var map = L.map('mapid').setView([37.0902, -95.7129], 4);
+    map = L.map('mapid').setView([37.0902, -95.7129], 4);
 
     // Add OSM base tilelayer
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '© OpenStreetMap contributors'
     }).addTo(map);
 
-    // Load the StatePop data and add it to the map
-    getData(map);
+    // Call getData function for the year 2013
+    getData(map, "2013");
 }
 
-// Ensure `createMap` is called once DOM is fully loaded
+// Calculate the minimum value of the data
+function calculateMinValue(data, attribute) {
+    // Create empty array to store all data values
+    var allValues = [];
+    
+    // Loop through each feature
+    for (var feature of data.features) {
+        // Get the value for the specified year
+        var value = Number(feature.properties[attribute]);
+        // Add value to array
+        allValues.push(value);
+    }
+    
+    // Get minimum value of our array
+    var minValue = Math.min(...allValues);
+    
+    return minValue;
+}
+
+// Calculate the radius of each proportional symbol
+function calcPropRadius(attValue) {
+    // Constant factor adjusts symbol sizes evenly
+    var minRadius = 5;
+    // Flannery Appearance Compensation formula
+    var radius = 1.0083 * Math.pow(attValue / minValue, 0.5715) * minRadius;
+    
+    return radius;
+}
+
+// Step 3: Add circle markers for point features to the map
+function createPropSymbols(data, attribute) {
+    // Calculate minimum data value for the specified year
+    minValue = calculateMinValue(data, attribute);
+
+    // Create marker options
+    var geojsonMarkerOptions = {
+        fillColor: "#ff7800",
+        color: "#fff",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+    };
+
+    L.geoJSON(data, {
+        pointToLayer: function (feature, latlng) {
+            // For each feature, determine its value for the selected attribute
+            var attValue = Number(feature.properties[attribute]);
+
+            // Give each feature's circle marker a radius based on its attribute value
+            geojsonMarkerOptions.radius = calcPropRadius(attValue);
+
+            // Create circle markers
+            return L.circleMarker(latlng, geojsonMarkerOptions);
+        },
+        onEachFeature: onEachFeature
+    }).addTo(map);
+}
+
+// Step 2: Import GeoJSON data
+function getData(map, attribute) {
+    // Load the data
+    fetch("data/StatePopChange.geojson")
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(json) {
+            // Call function to create proportional symbols
+            createPropSymbols(json, attribute);
+        });
+}
+
+// Ensure createMap is called once DOM is fully loaded
 document.addEventListener('DOMContentLoaded', createMap);
+
+// Create a function to attach popups to each mapped feature
+function onEachFeature(feature, layer) {
+    // Create HTML string with all properties
+    var popupContent = "";
+    if (feature.properties) {
+        // Loop to add feature property names and values to HTML string
+        for (var property in feature.properties) {
+            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
+        }
+        layer.bindPopup(popupContent);
+    }
+}
